6ccc11b4fe8971fb5612fa7f6ebe4c71a838eb2d
Lucas M Lima <lucas.mlima@hotmail.com.br>
2016-01-15 22:46:41 -0300

    Project

---
+++ Assets/Scripts/EventCalculate.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class EventCalculate : MonoBehaviour {
+
+    public InputField inputValue;
+    public Text result;
+
+	public void eventCalculate()
+    {
+        try
+        {
+            int valueCount = int.Parse(inputValue.text);
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < valueCount; i++);
+            long elapsedMs = watch.ElapsedMilliseconds;
+            //watch.Stop();
+            
+            result.text = "Resultado: "+elapsedMs;
+        }
+        catch (ExitGUIException e) { }
+    }
+}
---
+++ Assets/Scripts/EventReset.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EventReset : MonoBehaviour {
+
+    public InputField inputValue;
+    public Text result;
+
+	public void eventReset()
+    {
+        inputValue.text = "";
+        result.text = "| Resultado |";
+    }
+}
